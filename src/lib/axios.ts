309a6fc63f8433af16c29c56c224b77fd29b027d
Micheal <michealobarewon2015@gmail.com>
2025-11-26 15:18:27 +0100

    build error

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,9 +4,6 @@ const apiUrl = import.meta.env.VITE_API_URL;
 export const api = axios.create({
   baseURL: apiUrl,
   timeout: 120000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 // Request interceptor
